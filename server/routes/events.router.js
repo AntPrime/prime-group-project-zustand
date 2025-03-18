0321363d9466adb38cbfbd6085aaf7345eb28df6
Gregg Trunnell <gggtrunnell@gmail.com>
2025-03-18 13:03:10 -0500

    got table data to database.sql

--- server/routes/events.router.js
+++ server/routes/events.router.js
@@ -7,8 +7,11 @@ const userStrategy = require('../strategies/user.strategy');
 const router = express.Router();
 
 router.get('/', (req, res)=>{
-  const queryText = `SELECT events.id AS "events id", 
-activities.activity AS "activity", 
+  const queryText = `
+SELECT events.id AS "events id", 
+events.title AS "title",
+categories.activity AS "activity", 
+events.date AS "date",
 events.location AS "location",  
 events.home_team AS "home team", 
 events.away_team AS "away team", 
@@ -18,22 +21,38 @@ camera.username AS "camera",
 producer.username AS "producer",
 events.channel AS "channel", 
 events.notes AS "notes"
-FROM events
-LEFT JOIN activities ON events.activities_id = activities.id
-LEFT JOIN users AS "pbp_user" ON pbp_user.id = events.play_by_play
-LEFT JOIN users AS "color_comm_user" ON color_comm_user.id = events.color_commentator
-LEFT JOIN users AS "camera" ON camera.id = events.camera
-LEFT JOIN users AS "producer" ON producer.id = events.producer
-ORDER BY events.id; `
+FROM "events"
+LEFT JOIN "categories" ON events.activities_id = categories.id
+LEFT JOIN "users" AS "pbp_user" ON pbp_user.id = events.play_by_play
+LEFT JOIN "users" AS "color_comm_user" ON color_comm_user.id = events.color_commentator
+LEFT JOIN "users" AS "camera" ON camera.id = events.camera
+LEFT JOIN "users" AS "producer" ON producer.id = events.producer
+ORDER BY events.id;`;
   pool.query(queryText)
   .then((results)=>{
     console.log("results from db", results.rows)
     res.send(results.rows)
   })
   .catch((err)=>{
-    console.log("error in test.router get", err)
+    console.log("error in events.router get", err)
     res.sendStatus(400);
   })
 })
 
+router.post('/', (req, res)=>{
+  const queryText = `
+INSERT INTO "events" ("activities_id","title", "date","time","home_team","away_team","location", "channel","notes")
+VALUES 
+('1','Event title1', '2025-03-13','14:30:00','Elk River','Rogers','Elks','RogersTv','Look out for pickleballers');
+`;
+  pool.query(queryText)
+  .then((results)=>{
+    console.log("post to db", results)
+    res.send(results)
+  })
+  .catch((err)=>{
+    console.log("error in events.router post", err)
+    res.sendStatus(400);
+  })
+})
 module.exports = router;
